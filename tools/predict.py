a0731a1ef9e335bc018872fffec3bc2f0ed78cf5
Frank Lu <frankz.lu@googlemail.com>
2018-11-23 22:01:16 +0100

    accuracy and validation set

--- tools/predict.py
+++ tools/predict.py
@@ -4,3 +4,9 @@ def predict(model, test_data):
 
 def train(model, train_data, labels):
     return model.train(train_data, labels)
+
+def save( filename, header, data_ids, data_labels ):
+    import pandas as pd
+    submission = pd.DataFrame({header[0]: data_ids, header[1]: data_labels})
+    submission.to_csv(filename, index=False)
+    print("Saved file: " + filename)
